79896e497cf6784c0eb22c1f0dfb5e9a4c7b13c3
Rasmus Lindahl <rasmus.lindahl1996@gmail.com>
2025-03-07 13:27:08 +0100

    Refactor code

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ClientWrapper from './components/ClientWrapper';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <ClientWrapper />
-    </main>
-  );
-} 
